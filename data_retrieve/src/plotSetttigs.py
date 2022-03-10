77bf2714b9e03fd380c9631f640ed58801124155
razielar <razielar@gmail.com>
2022-03-10 17:42:13 +0100

    retrieve sp500 companies workds

---
+++ data_retrieve/src/plotSetttigs.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Plot parameters:
+sns.set_context("paper", font_scale= 1.5)
+plt.rcParams['axes.spines.right']= False
+plt.rcParams['axes.spines.top']= False
+plotsize = (18, 5)
+plt.rcParams['figure.figsize']= plotsize
